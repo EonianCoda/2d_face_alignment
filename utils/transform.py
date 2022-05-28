9701e7f4be0530d152bdff8cc9c63f681ccbeecf
llooww10120 <llooww10120@gmail.com>
2022-05-29 03:37:34 +0800

    fix flip bug

--- utils/transform.py
+++ utils/transform.py
@@ -5,9 +5,9 @@ import random
 class RandomHorizontalFlip(object):
     def __init__(self, flip_x=0.5):
         self.flip_x = flip_x
-        self.mapping = [[0, 1, 2, 3, 4, 5, 6, 7, 17, 18, 19, 20, 21, 36, 37, 38, 39, 41, 40, 31, 32, 50, 49, 48, 61, 62, 67, 59, 58], 
-                        [16, 15, 14, 13, 12, 11, 10, 9, 26, 25, 26, 23, 22, 45, 44, 43, 42, 46, 47, 35, 34, 52, 53, 54, 63, 64, 65, 55, 56]]
-        self.do_mapping = False
+        self.mapping = [[0, 1, 2, 3, 4, 5, 6, 7, 17, 18, 19, 20, 21, 36, 37, 38, 39, 41, 40, 31, 32, 50, 49, 48, 61, 60, 67, 59, 58], 
+                        [16, 15, 14, 13, 12, 11, 10, 9, 26, 25, 24, 23, 22, 45, 44, 43, 42, 46, 47, 35, 34, 52, 53, 54, 63, 64, 65, 55, 56]]
+        self.do_mapping = True
     def __call__(self, img, label:torch.Tensor, gt_label:torch.Tensor):
         """
         Args:
@@ -59,14 +59,14 @@ class Transform(object):
     def __call__(self, img, label, gt_label):
         
         # Random flip
-        if self.is_train:
-            img, label, gt_label = self.random_flip(img, label, gt_label)
+        # if self.is_train:
+        #     img, label, gt_label = self.random_flip(img, label, gt_label)
 
         img = transforms.ToTensor()(img)
 
         # # Random noise
-        # if self.is_train:
-        #     img = self.random_noise(img)
+        if self.is_train:
+            img = self.random_noise(img)
 
         img = self.normalize(img)
         return img, label, gt_label
