c62682db161882b07809456cc3980553f1062f9c
EonianCoda <textuseful159000@gmail.com>
2022-05-28 21:22:22 +0800

    move plot_keyporint from tool to visualize

--- utils/tool.py
+++ utils/tool.py
@@ -9,44 +9,10 @@ from tqdm import tqdm
 import random
 import os
 import time
-import cv2
+
 def mkdir_if_exist(path:str):
     if not os.path.isdir(path):
         os.mkdir(path)
-
-def draw_point(im, coord:tuple, color:tuple, text:str=None):
-    if text != None:
-        text_coord = (coord[0] - 5 * len(text), coord[1] - 5)
-        cv2.putText(im,
-                    text,
-                    text_coord,
-                    cv2.FONT_HERSHEY_PLAIN,
-                    1,
-                    color)
-    cv2.circle(im, 
-                center=coord, 
-                radius=3, 
-                color=color, 
-                thickness=-1)
-
-def plot_keypoint(img_path:str, gt:torch.Tensor, pred:torch.Tensor, show_index:bool=True, show_line:bool=True):
-    im = cv2.imread(os.path.join("./data/val", img_path))
-
-    # draw points
-    for i, ((gt_x, gt_y), (pred_x, pred_y)) in enumerate(zip(gt, pred)):
-        text = str(i+1) if show_index else None
-        draw_point(im, 
-                    text=text,
-                    coord=(gt_x, gt_y), 
-                    color=(255, 0, 0))
-        draw_point(im, 
-                    text=text, 
-                    coord=(pred_x, pred_y), 
-                    color=(0, 0, 255))
-        if show_line:
-            cv2.line(im, (gt_x, gt_y), (pred_x, pred_y), color=(0,255,0), thickness=1)
-
-    return im
 
 
 class Warmup_ReduceLROnPlateau(_LRScheduler):
