simplify training process, add weight map, option for fix_coord,  and fix rotation bug

eval.py
@@ -16,6 +16,7 @@ def main():
     parser.add_argument('--type', type=str, default="val")
     args = parser.parse_args()
 
+    fix_coord = cfg['fix_coord']
     ### path ###
     annot_path = f"./data/{args.type}_annot.pkl"
     data_path = f"./data/{args.type}"
@@ -24,12 +25,9 @@ def main():
     model_type = cfg['model_type'][cfg['model_type_idx']]
     if model_type == "classifier":
         cfg.update(classifier_cfg)
-        num_HG = cfg['num_HG']
-        HG_depth = cfg['HG_depth']
     elif model_type == "regressor":
         cfg.update(regressor_cfg)
-        backbone = cfg['backbone'][cfg['backbone_idx']]
-        dropout = cfg['dropout']
+
 
     batch_size = cfg['batch_size'] * 2
 
@@ -41,7 +39,7 @@ def main():
     model = get_model(cfg)
 
     load_parameters(model, model_path)
-    test_NME_loss, test_NME_loss_68 = val(model, test_loader, device, model_type)
+    test_NME_loss, test_NME_loss_68 = val(model, test_loader, device, model_type, fix_coord=fix_coord)
     print(f"Average NME Loss : {test_NME_loss:.4f}")
     plot_loss_68(test_NME_loss_68)
     print(np.argsort(test_NME_loss_68))

utils/dataset.py
@@ -57,7 +57,8 @@ def process_annot(annot_path:str):
 
     #     return images, gt_labels, kernels
 
-def get_train_val_dataset(data_root:str, annot_path:str, train_size=0.8, use_image_ratio=1.0, model_type="classifier",aug_setting:dict=None):
+def get_train_val_dataset(data_root:str, annot_path:str, train_size=0.8, use_image_ratio=1.0, model_type="classifier",
+                            aug_setting:dict=None, use_weight_map=False,fix_coord=False):
     """Get training set and valiating set
     Args:
         data_root: the data root for images
@@ -87,12 +88,18 @@ def get_train_val_dataset(data_root:str, annot_path:str, train_size=0.8, use_ima
                                     images=train_images,
                                     labels=train_labels,
                                     model_type=model_type,
+                                    return_gt=False,
+                                    use_weight_map=use_weight_map,
+                                    fix_coord=fix_coord,
                                     transform='train',
                                     aug_setting=aug_setting)
     val_dataset = FaceSynthetics(data_root=data_root, 
                                     images=val_images,
                                     labels=val_labels,
                                     model_type=model_type,
+                                    return_gt= True,
+                                    use_weight_map=use_weight_map,
+                                    fix_coord=fix_coord,
                                     transform='val')
     return train_dataset, val_dataset
 
@@ -102,6 +109,7 @@ def get_test_dataset(data_path:str, annot_path:str, model_type:str):
                                     images=images,
                                     labels=labels,
                                     model_type=model_type,
+                                    return_gt= True,
                                     transform='test')
     return test_dataset
 
@@ -216,7 +224,9 @@ class Old_heatmap_converter(object):
         return heatmap
 
 class FaceSynthetics(Dataset):
-    def __init__(self, data_root:str, images:list, labels:np.ndarray, transform="train", model_type="classifier", aug_setting:dict=None, heatmap_size=96) -> None:
+    def __init__(self, data_root:str, images:list, labels:np.ndarray, transform="train", 
+                model_type="classifier", aug_setting:dict=None, heatmap_size=96, return_gt=True, 
+                use_weight_map=False, fix_coord=False) -> None:
         """
         Args:
             data_root: the path of the data
@@ -230,6 +240,8 @@ class FaceSynthetics(Dataset):
         super(FaceSynthetics, self).__init__()
         self.data_root = data_root
         self.model_type = model_type
+        self.return_gt = return_gt
+        self.use_weight_map = use_weight_map
         # transform
         self.transform = get_transform(data_type=transform,
                                         aug_setting=aug_setting)
@@ -239,13 +251,15 @@ class FaceSynthetics(Dataset):
         self.num_classes = len(self.labels[0])
         # heatmap converter
         if self.model_type == "classifier":
-            self.converter = Heatmap_converter(heatmap_size)
+            if fix_coord:
+                self.converter = Heatmap_converter(heatmap_size)
+            else:
+                self.converter = Old_heatmap_converter(heatmap_size)
             # self.heatmap_size = heatmap_size
 
     def __len__(self):
         return len(self.images)
  
-  
     @staticmethod
     def _generate_weight_map(heatmap):
         weight_map = torch.zeros_like(heatmap)
@@ -259,11 +273,20 @@ class FaceSynthetics(Dataset):
         img_path = os.path.join(self.data_root, self.images[idx])
         im = Image.open(img_path)
         im, label = self.transform(im, self.labels[idx])
-        gt_label = label.clone()
+        if self.return_gt:
+            gt_label = label.clone()
         # transform point to heatmap
         if self.model_type == "classifier":
             label = self.converter.convert(label)
-        return im, label, gt_label
+
+        if self.return_gt and self.use_weight_map:
+            return im, label, gt_label, self._generate_weight_map(label)
+        elif self.return_gt:
+            return im, label, gt_label
+        elif self.use_weight_map:
+            return im, label, self._generate_weight_map(label)
+        else:
+            return im, label
 
 class Predicting_FaceSynthetics(Dataset):
     def __init__(self, data_root:str, images:list) -> None:

utils/tool.py
@@ -39,26 +39,25 @@ def load_parameters(model, path, optimizer=None, scheduler=None, model_type:str=
 
     print("End of loading !!!")
 
-def val(model, test_loader, device, model_type:str):
+def val(model, test_loader, device, model_type:str,fix_coord=False):
     print("Starting Validation....")
     
     model = model.to(device)
-    model.eval()
-    # criterion = nn.CrossEntropyLoss()
+    
     total_NME_loss = 0
     total_NEM_loss_68 = np.zeros(68)
     num_data = 0
 
-    for batch_idx, (data, label, gt_label) in enumerate(tqdm(test_loader)):
+    model.eval()
+    for data in tqdm(test_loader):
         with torch.no_grad():
-            data = data.to(device)
-            # label = label.to(device)
-            outputs = model(data)
-            # loss = 0
-            # for output in outputs:
-            #     loss += criterion(output, label)
+            img, _ , gt_label = data
+            img = img.to(device)
+
+            outputs = model(img)
+
             if model_type == "classifier":
-                pred = heatmap_to_landmark(outputs)
+                pred = heatmap_to_landmark(outputs,fix_coord=fix_coord)
             elif model_type == "regressor":
                 pred = outputs.detach().cpu()
 
@@ -67,12 +66,30 @@ def val(model, test_loader, device, model_type:str):
             total_NME_loss += pred_loss
             total_NEM_loss_68 += pred_loss_68.sum(axis=0)
     print("End of validating....")
-    # print(f"Average NME Loss : {total_NME_loss / num_data:.4f}")
+
 
     return (total_NME_loss / num_data), (total_NEM_loss_68 / num_data)
 
-def train(model, train_loader, val_loader, test_loader, epoch:int, save_path:str, device, criterion, 
-    scheduler, optimizer, model_type:str, loss_type:str, exp_name="", train_hyp:dict=dict(), only_save_best=False, resume_epoch=-1):
+def process_loss(model_type:str, loss_type:str, criterion, outputs:torch.Tensor, label:torch.Tensor, weight_map:torch.Tensor=None)
+    loss = 0
+    if model_type == "classifier":
+        if loss_type =="L2":
+            num_target = (label != 0).sum()
+            for output in outputs:
+                loss += criterion(output, label) / num_target
+        elif loss_type == "wing_loss":
+            for output in outputs:
+                loss += criterion(output, label)
+        elif loss_type == "weighted_L2":
+            if weight_map == None:
+                raise ValueError("Weight map cannot be None!")
+            for output in outputs:
+                loss += criterion(output, label, weight_map)
+    elif model_type == "regressor":
+        loss += criterion(outputs, label)
+    return loss
+def train(model, train_loader, val_loader, test_loader, epoch:int, save_path:str, device, criterion, scheduler, optimizer, 
+        model_type:str, loss_type:str, exp_name="", train_hyp:dict=dict(), only_save_best=False, resume_epoch=-1,fix_coord=False):
     start_train = time.time()
     # Print training information
     print("Start training!!")
@@ -80,7 +97,8 @@ def train(model, train_loader, val_loader, test_loader, epoch:int, save_path:str
     if model_type == "regressor":
         print(f"Backbone type = {model_type}")
     print(f"Length of training dataloader = {len(train_loader)}")
-    
+    print(f"Loss type = {loss_type}")
+
     # Create writerr for recording loss
     if exp_name == "":
         writer = SummaryWriter()
@@ -93,31 +111,27 @@ def train(model, train_loader, val_loader, test_loader, epoch:int, save_path:str
     os.makedirs(save_path,exist_ok=True)
 
     # Best NME loss and epoch for save best .pt
-    best_train_NME_loss = 999
     best_val_NME_loss = 999
     best_test_NME_loss = 999
-    best_train_epoch = 0
     best_val_epoch = 0
     best_test_epoch = 0
 
+    use_weight_map = (loss_type == "weighted_L2")
+
     # Set start epoch and end epoch
     if resume_epoch != -1:
+        print("Resume training!!")
         start_epoch = resume_epoch
         end_epoch = resume_epoch + epoch
-        global_training_step = len(train_loader) * (resume_epoch - 1)
-        global_validation_step = len(val_loader) * (resume_epoch - 1)
-        print("Resume training!!")
     else:
         start_epoch = 1
         end_epoch = epoch + 1
-        global_training_step = 0
-        global_validation_step = 0
+
     # Starting training
     for epoch in range(start_epoch, end_epoch):
         print(f'epoch = {epoch}')
         start_time = time.time()
-        train_loss = 0.0
-        train_NME_loss = 0.0
+
 
         # Record learning rate
         writer.add_scalar(tag="hyperparameters/lr",
@@ -126,111 +140,83 @@ def train(model, train_loader, val_loader, test_loader, epoch:int, save_path:str
         
         # Training part
         model.train()
-        for batch_idx, (data, label, gt_label) in enumerate(tqdm(train_loader)):
-            data = data.to(device)
+        train_loss = 0.0
+        for data in tqdm(train_loader):
+            if use_weight_map:
+                img, label, weight_map = data
+                weight_map = weight_map.to(device)
+            else:
+                img, label = data
+                weight_map = None
+
+            # Forward part
+            img = img.to(device)
             label = label.to(device)
-            outputs = model(data) 
-            # intermediate supervision
-            loss = 0
-            if model_type == "classifier":
-                if loss_type =="L2":
-                    num_target = (label != 0).sum()
-                    for output in outputs:
-                        loss += criterion(output, label) / num_target
-                elif loss_type == "wing_loss":
-                    for output in outputs:
-                        loss += criterion(output, label)
-            elif model_type == "regressor":
-                loss += criterion(outputs, label)
+            outputs = model(img)
+            # Calculate Loss
+            loss = process_loss(model_type, loss_type, criterion, outputs, label, weight_map)
+
+            # Backward and update
             train_loss += loss.item()
             optimizer.zero_grad()
             loss.backward()
             nn.utils.clip_grad_norm_(model.parameters(), max_norm= 5.)
             optimizer.step()
-            writer.add_scalar(tag="train/step_loss",
-                            scalar_value=float(loss),
-                            global_step=global_training_step)
-            
-            # Calculate gt loss with groud truth label
-            if model_type == "classifier":
-                pred_label = heatmap_to_landmark(outputs)
-            else:
-                pred_label = outputs.detach().cpu()
-
-            NME_loss = NME(pred_label, gt_label)
-            writer.add_scalar(tag="train/NME_step_loss",
-                            scalar_value=float(NME_loss), 
-                            global_step=global_training_step)
-            train_NME_loss += NME_loss
-            global_training_step += 1
-
+         
             del loss
   
+        # Recording Epoch loss with tensorboard
         train_loss /= len(train_loader.dataset)
-        train_NME_loss /= len(train_loader.dataset)
-        # Record loss with tensorboard
         writer.add_scalar(tag="train/epoch_loss",
                         scalar_value=float(train_loss), 
                         global_step=epoch)
-        writer.add_scalar(tag="train/NME_epoch_loss",
-                        scalar_value=float(train_NME_loss), 
-                        global_step=epoch)
+
         
         # validation part 
         with torch.no_grad():
             model.eval()
             val_loss = 0.0
             val_NME_loss = 0.0
-            for batch_idx, (data, label, gt_label) in enumerate(tqdm(val_loader)):
-                data = data.to(device)
+            for data in tqdm(val_loader):
+                if use_weight_map:
+                    img, label, gt_label, weight_map = data
+                    weight_map = weight_map.to(device)
+                else:
+                    img, label, gt_label = data
+                    weight_map = None
+
+                # Forward part
+                img = img.to(device)
                 label = label.to(device)
+                outputs = model(img)
 
-                outputs = model(data)
                 # intermediate supervision
-                loss = 0
-                if model_type == "classifier":
-                    if loss_type =="L2":
-                        num_target = (label != 0).sum()
-                        for output in outputs:
-                            loss += criterion(output, label) / num_target
-                    elif loss_type == "wing_loss":
-                        for output in outputs:
-                            loss += criterion(output, label)
-                elif model_type == "regressor":
-                    loss += criterion(outputs, label)
+                loss = process_loss(model_type, loss_type, criterion, outputs, label, weight_map)
                 val_loss += loss.item()
-                writer.add_scalar(tag="val/step_loss",
-                            scalar_value=float(loss), 
-                            global_step=global_validation_step)
-
                 # Calculate loss with groud truth label
                 if model_type == "classifier":
-                    pred_label = heatmap_to_landmark(outputs)
-                else:
+                    pred_label = heatmap_to_landmark(outputs, fix_coord=fix_coord)
+                elif model_type == "regressor":
                     pred_label = outputs.detach().cpu()
 
-                NME_loss =  NME(pred_label, gt_label)
-                writer.add_scalar(tag="val/NME_step_loss",
-                                scalar_value=float(NME_loss), 
-                                global_step=global_validation_step)
-                val_NME_loss += NME_loss
-                global_validation_step += 1
+                val_NME_loss += NME(pred_label, gt_label)
+
             
             val_loss /= len(val_loader.dataset)
             val_NME_loss /= len(val_loader.dataset)
             writer.add_scalar(tag="val/NME_epoch_loss",
-                scalar_value=float(val_NME_loss), 
-                global_step=epoch)
+                            scalar_value=float(val_NME_loss), 
+                            global_step=epoch)
             writer.add_scalar(tag="val/epoch_loss",
                             scalar_value=float(val_loss), 
                             global_step=epoch)
         # Testing part
-        test_NME_loss, test_NME_loss_68 = val(model, test_loader, device, model_type)
+        test_NME_loss, test_NME_loss_68 = val(model, test_loader, device, model_type, fix_coord=fix_coord)
         writer.add_scalar(tag="test/NME_loss",
                                 scalar_value=float(test_NME_loss), 
                                 global_step=epoch)
         # Scheduler steping
-        scheduler.step(val_loss)
+        scheduler.step(val_NME_loss)
 
         # Display the results
         end_time = time.time()
@@ -239,15 +225,12 @@ def train(model, train_loader, val_loader, test_loader, epoch:int, save_path:str
         print('time = {} MIN {:.1f} SEC, total time = {} Min {:.1f} SEC '.format(elp_time // 60, elp_time % 60, (end_time-start_train) // 60, (end_time-start_train) % 60))
         formatted_str = "{: <20} : {:.6f}"
         print(formatted_str.format('Training loss', train_loss))
-        print(formatted_str.format('Training NME loss', train_NME_loss))
         print(formatted_str.format('Validating loss', val_loss))
         print(formatted_str.format('Validating NME loss', val_NME_loss))
         print(formatted_str.format('Testing NME loss', test_NME_loss))
         print('='*24 + '\n')
 
-        if train_NME_loss < best_train_NME_loss:
-            best_train_NME_loss = train_NME_loss
-            best_train_epoch = epoch
+
         if test_NME_loss < best_test_NME_loss:
             best_test_NME_loss = test_NME_loss
             best_test_epoch = epoch
@@ -263,17 +246,15 @@ def train(model, train_loader, val_loader, test_loader, epoch:int, save_path:str
 
     
     print("End of training !!!")
-    print(f"Best training NME loss {best_train_NME_loss:.6f} on epoch {best_train_epoch}")
     print(f"Best validating NME loss {best_val_NME_loss:.6f} on epoch {best_val_epoch}")
     print(f"Best testing NME loss {best_test_NME_loss:.6f} on epoch {best_test_epoch}")
 
-    metric_result = dict()
-    metric_result['best/train_NME_loss'] = best_train_NME_loss
-    metric_result['best/val_NME_loss'] = best_val_NME_loss
-    metric_result['best/val_epoch'] = best_val_epoch
-    metric_result['best/test_NME_loss'] = best_test_NME_loss
-    metric_result['best/test_epoch'] = best_test_epoch
-    metric_result['end epoch'] = end_epoch
+    # metric_result = dict()
+    # metric_result['best/val_NME_loss'] = best_val_NME_loss
+    # metric_result['best/val_epoch'] = best_val_epoch
+    # metric_result['best/test_NME_loss'] = best_test_NME_loss
+    # metric_result['best/test_epoch'] = best_test_epoch
+    # metric_result['end epoch'] = end_epoch
 
     # writer.add_hparams(train_hyp, metric_result)
 

utils/transform.py
@@ -35,19 +35,20 @@ class RandomRoation(object):
     def __call__(self, img, label:torch.Tensor):
         if random.random() < self.prob:
             h, w, c = self.img_shape
+            origin_label = label.clone()
             
             angle_i = random.randint(0, len(self.angle_list)-1)
             angle = self.angle_list[angle_i]
-            img = F.rotate(img, angle)
+            r_img = F.rotate(img, angle)
 
             rot_matrix = self.rot_matrices[angle_i]
 
-            r_label = self._rotate_points(label.float().clone(), w, h, rot_matrix) # Rotate label
+            r_label = self._rotate_points(label, w, h, rot_matrix) # Rotate label
             # Out of bound
             if (r_label < 0).any() or (r_label >= h).any():
-                return img, label
+                return img, origin_label
 
-            return img, r_label
+            return r_img, r_label
 
         return img, label
 