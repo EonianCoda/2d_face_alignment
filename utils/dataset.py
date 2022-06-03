8c179196f5d6cd37b8e9648a058f23be7b5e5462
EonianCoda <textuseful159000@gmail.com>
2022-06-03 18:27:36 +0800

    add Pose-base data balancing

--- utils/dataset.py
+++ utils/dataset.py
@@ -2,6 +2,11 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 from torchvision.transforms import transforms
+
+from sklearn.decomposition import PCA
+from scipy.spatial import procrustes
+from scipy import ndimage
+
 from PIL import Image
 import numpy as np
 from platform import python_version
@@ -9,10 +14,9 @@ import os
 import random
 import math
 import copy
-import pickle
 
-from scipy import ndimage
 from utils.transform import get_transform
+from utils.convert_tool import is_None
 
 def process_annot(annot_path:str):
     """Read the annot file and process label(e.g. discard wrong label)
@@ -32,6 +36,7 @@ def process_annot(annot_path:str):
     labels = labels[valid_idxs]
     images = [images[i] for i in valid_idxs]
     return images, labels
+
     # For classifier
     # Generate kernel
     # path = os.path.dirname(annot_path)
@@ -58,7 +63,7 @@ def process_annot(annot_path:str):
     #     return images, gt_labels, kernels
 
 def get_train_val_dataset(data_root:str, annot_path:str, train_size=0.8, use_image_ratio=1.0, model_type="classifier",
-                            aug_setting:dict=None, use_weight_map=False,fix_coord=False):
+                            aug_setting:dict=None, use_weight_map=False,fix_coord=False, get_weight=False):
     """Get training set and valiating set
     Args:
         data_root: the data root for images
@@ -67,7 +72,6 @@ def get_train_val_dataset(data_root:str, annot_path:str, train_size=0.8, use_ima
         use_image_ratio: how many images to use in training and validation
     """
     images, labels = process_annot(annot_path)
-    
     # Split train/val set
     idxs = [i for i in range(int(len(images) * use_image_ratio))]
     random.shuffle(idxs)
@@ -77,12 +81,17 @@ def get_train_val_dataset(data_root:str, annot_path:str, train_size=0.8, use_ima
     train_images = [images[i] for i in train_idxs]
     train_labels = labels[train_idxs]
 
-
     # Validation set
     val_idxs = idxs[int(len(idxs)*train_size): ]
     val_images = [images[i] for i in val_idxs]
     val_labels = labels[val_idxs]
 
+    if get_weight:
+        pdb = PDB(annot_path)
+        weights = pdb.get_weights(labels)
+        train_weights = weights[train_idxs]
+    else:
+        train_weights = None
 
     train_dataset = FaceSynthetics(data_root=data_root, 
                                     images=train_images,
@@ -91,6 +100,7 @@ def get_train_val_dataset(data_root:str, annot_path:str, train_size=0.8, use_ima
                                     return_gt=False,
                                     use_weight_map=use_weight_map,
                                     fix_coord=fix_coord,
+                                    data_weight = train_weights,
                                     transform='train',
                                     aug_setting=aug_setting)
     val_dataset = FaceSynthetics(data_root=data_root, 
@@ -117,6 +127,73 @@ def get_pred_dataset(data_path:str):
     images = os.listdir(data_path)
     test_dataset = Predicting_FaceSynthetics(data_root=data_path, images=images)
     return test_dataset
+
+class PDB(object):
+    """Pose-based data balancing
+    """
+    def __init__(self, annot_path:str):
+        path = os.path.dirname(annot_path)
+        file_name = os.path.basename(annot_path).split('.')[0]
+        cached_file = f'cached_{file_name}_projected.pkl'
+        self.cached_file = os.path.join(path, cached_file)
+
+    def _cal_projected(self, labels):
+        import pickle
+        # Load cached file
+        if os.path.isfile(self.cached_file):
+            self.projected = pickle.load(open(self.cached_file, 'rb'))
+            return
+
+        if isinstance(labels, torch.Tensor):
+            labels = labels.clone()
+        elif isinstance(labels, np.ndarray):
+            labels = labels.copy()
+
+        print("Calculating projected....")
+        shapes = labels
+        ref_shape = shapes.mean(dim=0)
+        aligned = []
+        for shape in shapes:
+            _ , transform , _ = procrustes(ref_shape, shape)
+            aligned.append(transform)
+        aligned = np.stack(aligned)
+
+        b, n, c = aligned.shape # (batch_size, num_landmark, coordinate)
+        pca = PCA(n_components=1)
+        self.projected = pca.fit_transform(aligned.reshape((-1, n*c)))
+        self.projected = self.projected[:,0]
+        print("End of calculating projected....")
+
+        pickle.dump(self.projected, open(self.cached_file, 'wb'))
+
+    def get_weights(self, labels):
+        # Calculating projected
+        self._cal_projected(labels)
+        num_data = len(self.projected)
+        rank = np.argsort(self.projected)
+        value_rank = np.sort(self.projected)
+
+        bins = [-0.4, -0.3, 0.3, 0.4]
+        indexs = [(value_rank<bin).sum() for bin in bins]
+        indexs.append(num_data)
+        # num_data = [indexs[0],
+        #             indexs[1] - indexs[0], 
+        #             indexs[2] - indexs[1],
+        #             indexs[3] - indexs[2],
+        #             len(projected) - indexs[3]]
+
+        # ratio = [num / len(projected) for num in num_data]
+        # weight = 1 / (ratio / ratio[2])
+        W = [3,2,1,2,3]
+        weights = np.zeros((num_data))
+        
+        cur_idx = 0
+        for i, w in enumerate(W):
+            target_index = rank[cur_idx: indexs[i]]
+            weights[target_index] = w
+            cur_idx = indexs[i]
+
+        return weights
 
 class Heatmap_converter(object):
     def __init__(self, heatmap_size=96, window_size=7, sigma=1.75):
@@ -226,7 +303,7 @@ class Old_heatmap_converter(object):
 class FaceSynthetics(Dataset):
     def __init__(self, data_root:str, images:list, labels:np.ndarray, transform="train", 
                 model_type="classifier", aug_setting:dict=None, heatmap_size=96, return_gt=True, 
-                use_weight_map=False, fix_coord=False) -> None:
+                use_weight_map=False, fix_coord=False, data_weight=None) -> None:
         """
         Args:
             data_root: the path of the data
@@ -249,6 +326,20 @@ class FaceSynthetics(Dataset):
         self.images = images
         self.labels= torch.tensor(labels)
         self.num_classes = len(self.labels[0])
+
+        # data weight
+        if not is_None(data_weight):
+            max_ratio = 0.2
+            num_imgs = len(self.images)
+            total_weight = int(data_weight.sum())
+            self.extra_num_data = min(total_weight - num_imgs, int(max_ratio * num_imgs))
+            self.num_data = num_imgs + self.extra_num_data
+            self.max_idx = len(self.images)
+            self.data_weight = data_weight / data_weight.sum()
+        else:
+            self.num_data = len(self.images)
+            self.extra_num_data = 0
+
         # heatmap converter
         if self.model_type == "classifier":
             if fix_coord:
@@ -258,8 +349,17 @@ class FaceSynthetics(Dataset):
             # self.heatmap_size = heatmap_size
 
     def __len__(self):
-        return len(self.images)
- 
+        return self.num_data
+    
+    def index_mapping(self, idx:int):
+        if self.extra_num_data == 0:
+            return idx
+        else:
+            if idx >= self.max_idx:
+                idx = np.random.choice(range(self.max_idx), p=self.data_weight)
+                return int(idx)
+            else:
+                return idx
     @staticmethod
     def _generate_weight_map(heatmap):
         weight_map = torch.zeros_like(heatmap)
@@ -269,6 +369,7 @@ class FaceSynthetics(Dataset):
             weight_map[i][dilate>0.2] = 1
         return weight_map
     def __getitem__(self, idx:int):
+        idx = self.index_mapping(idx)
         # Read imagee
         img_path = os.path.join(self.data_root, self.images[idx])
         im = Image.open(img_path)
