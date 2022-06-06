765013a52396f31ef93b6d1938c537eeaee59721
llooww10120 <llooww10120@gmail.com>
2022-06-07 01:27:51 +0800

    fix scheduler bug

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,8 +1,6 @@
 from torch.optim.lr_scheduler import _LRScheduler, ReduceLROnPlateau
 import math
 
-
-
 class Warmup_ReduceLROnPlateau(_LRScheduler):
     def __init__(self, optimizer, warm_up_step, patience=3, verbose=True):
         self.warm_up_step = warm_up_step
@@ -12,15 +10,16 @@ class Warmup_ReduceLROnPlateau(_LRScheduler):
         
         self.last_step = 0
         self.ratio = self.warm_up_step / 20
+        self.flag = False
         super(Warmup_ReduceLROnPlateau, self).__init__(optimizer)
     def get_lr(self) -> float:
-        if self.last_step < self.warm_up_step:
+        if self.last_step <= self.warm_up_step:
             cur_ratio = math.ceil(self.last_step / self.ratio) / (self.warm_up_step / self.ratio)
             cur_ratio = max(cur_ratio, 0.5 / (self.warm_up_step / self.ratio))
             return [base_lr * cur_ratio for base_lr in self.base_lrs]
         else:
-            return self.after_scheduler._last_lr
 
+            return self.after_scheduler.get_last_lr()
     def _print_info(self):
         for group_idx, lr in enumerate(self.get_lr()):
             print("step {:4d}: Adjusting learning rate of group {} to {:4e}.".format(self.last_step, group_idx, lr))
@@ -33,7 +32,3 @@ class Warmup_ReduceLROnPlateau(_LRScheduler):
                 param_group['lr'] = lr
         elif metric != None:
             self.after_scheduler.step(metric)
-        # else:
-        #     raise ValueError("Currnet step is larger than warm up step and metirc still is None")
-        # if self.verbose and cur_lr != self.get_lr():
-        #     self._print_info()
